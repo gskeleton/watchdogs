c0977f19e80b0e7eaa7a6f4e2aefff2026463072
skeleton@dog <skeleton@noreply.dog>
2026-01-30 09:20:59 +0700

    __windows.cmd : remove require admin access

--- __windows.cmd
+++ __windows.cmd
@@ -1,13 +1,6 @@
 @echo off
 setlocal
 
-net session >nul 2>&1
-if %errorLevel% neq 0 (
-    echo Requesting administrator privileges...
-    powershell -Command "Start-Process '%~f0' -Verb RunAs"
-    exit /b
-)
-
 dir "%USERPROFILE%\Downloads"
 
 echo.
